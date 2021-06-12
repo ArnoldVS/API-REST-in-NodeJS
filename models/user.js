745306d73ac08ab79df85f2e32c3188696347b18
Arnold Valenzuela Sanchez <avs.guy.7@gmail.com>
2021-06-11 23:07:09 -0500

    adicion de  modelo User con Mongoose Schemas

---
+++ models/user.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt-nodejs')
+const crypto = require('crypto')
+
+const Schema = mongoose.Schema
+const { use } = require('../routes')
+
+const UserSchema = new Schema({
+    email: { type: String, unique: true, lowercase: true },
+    displayName: String,
+    avatar: String,
+    password: { type: String, select: false },
+    signupDate: { type: Date, default: Date.now() },
+    lastLogin: Date
+})
+
+// Encriptar la contrasena
+
+UserSchema.pre('save', (next) => {
+    let user = this
+    if (!user.isModified('password')) return next()
+
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) return next(err)
+
+        bcrypt.hash(use.password, salt, null, (err, hash) => {
+            if (err) return next(err)
+
+            user.password = hash
+            next()
+        })
+    })
+})
+
+UserSchema.method.gravatar = function() {
+    if (!this.email) return 'http://gravatar.com/avatar/?s=200&d=retro'
+
+    const md5 = crypto.createHash('md5').update(this.email).digest('hex')
+    return `http://gravatar.com/avatar/${md5}?s=200&d=retro`
+}
+
+module.exports = mongoose.model('User', UserSchema)
