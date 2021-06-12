ed21b27de8503e147fd0d5deacb59e73c0e781ea
Arnold Valenzuela Sanchez <avs.guy.7@gmail.com>
2021-06-11 19:27:41 -0500

    separacion de los modulos

--- index.js
+++ index.js
@@ -1,84 +1,15 @@
 'use strict'
 
-const express = require('express')
-const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-
-const Product = require('./models/product')
-
-// correr el servidor
-const app = express()
+const app = require('./app')
 const port = process.env.PORT || 3003
-
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-app.get('/api/product', (req, res) => {
-    Product.find({}, (err, products) => {
-        if (err) return res.status(500).send({ message: `Error al realizar la peticion: ${err}` })
-        if (!products) return res.status(404).send({ message: `El producto no existe` })
-
-        res.status(500).send({ products })
-    })
-})
-
-app.get('/api/product/:productId', (req, res) => {
-    let productId = req.params.productId
-
-    Product.findById(productId, (err, product) => {
-        if (err) return res.status(500).send({ message: `Error al realizar la peticion: ${err}` })
-        if (!product) return res.status(404).send({ message: `El producto no existe` })
-
-        res.status(200).send({ product })
-    })
-})
-
-app.post('/api/product', (req, res) => {
-    console.log('POST /api/product')
-    console.log(req.body)
-
-    let product = new Product()
-    product.name = req.body.name
-    product.picture = req.body.picture
-    product.price = req.body.price
-    product.category = req.body.category
-    product.description = req.body.description
-
-    product.save((err, productStored) => {
-        if (err) res.status(500).send({ message: `Error al guardar en la base de datos: ${err}` })
-
-        res.status(200).send({ product: productStored })
-    })
-})
-
-app.put('/api/product/:productId', (req, res) => {
-    let productId = req.params.productId
-    let update = req.body
-
-    Product.findByIdAndUpdate(productId, update, (err, productUpdate) => {
-        if (err) res.status(500).send({ message: `Error al actualizar el producto: ${err}` })
-
-        res.status(200).send({ product: productUpdate })
-    })
-})
-
-app.delete('/api/product/:productId', (req, res) => {
-    let productId = req.params.productId
-
-    Product.findById(productId, (err, product) => {
-        if (err) res.status(500).send({ message: `Error al borrar el producto: ${err}` })
-
-        product.remove((err, productId) => {
-            if (err) res.status(500).send({ mesage: `Error al borrar el producto: ${err}` })
-            res.status(200).send({ message: 'EL producto ha sido eliminado' })
-        })
-    })
-})
 
 // Inicializar la base de datos 
 mongoose.set('useNewUrlParser', true)
 mongoose.set('useUnifiedTopology', true)
 mongoose.set('useFindAndModify', false)
+
+// base de datos
 mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
     if (err) {
         return console.log(`Error al conectar a la base de datos: ${err}`)
